5ed57a0e367fb3dd91f348fa6e64ad52020e51b9
yasiinakbulut58 <94223290+yasiinakbulut58@users.noreply.github.com>
2021-12-17 22:49:04 +0300

    add vue-smooth-dnd library

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,7 @@ declare module "*.vue" {
   import Vue from "vue";
   export default Vue;
 }
+declare module "vue-smooth-dnd" {
+  import { Container, Draggable } from "vue-smooth-dnd";
+  export { Container, Draggable };
+}
