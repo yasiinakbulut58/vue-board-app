ef7c1e8ffb54ad1bfce47c962450c6339685304a
yasiinakbulut58 <94223290+yasiinakbulut58@users.noreply.github.com>
2021-12-18 11:12:16 +0300

    add poppins font and variables, move css in component to app.less

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,3 @@ export default {
   },
 };
 </script>
-
-<style lang="less">
-#app {
-  font-family: Poppins;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  margin-top: 60px;
-}
-</style>
